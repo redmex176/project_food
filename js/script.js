c50ec2de16fcc87feec485122a20caf8777fd496
Alex <xomutof.alex176@gmail.com>
2022-07-11 15:02:32 +0300

    added slider(carousel)

--- js/script.js
+++ js/script.js
@@ -196,19 +196,25 @@ window.addEventListener('DOMContentLoaded', () => {
     const getResource = async (url) => {
         const result = await fetch(url);
 
-        if(!result.ok){
-           throw new Error(`Could not fetch ${url}, status: ${result.status}`);
+        if (!result.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${result.status}`);
         }
 
         return await result.json();
     };
 
     getResource('http://localhost:3000/menu')
-    .then(data => {
-        data.forEach(({img, altimg, title, descr, price}) => {
-            new MenuCard(img, altimg, title, descr,price, '.menu .container', 'menu__item').render();
+        .then(data => {
+            data.forEach(({
+                img,
+                altimg,
+                title,
+                descr,
+                price
+            }) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container', 'menu__item').render();
+            });
         });
-    });
 
 
 
@@ -225,7 +231,7 @@ window.addEventListener('DOMContentLoaded', () => {
     forms.forEach((item) => {
         BindPostData(item);
     });
-    const postData = async (url,data) => {
+    const postData = async (url, data) => {
         const result = await fetch(url, {
             method: 'POST',
             headers: {
@@ -253,18 +259,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
             const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            postData('http://localhost:3000/requests',json)
-            .then(data => {
-                console.log(data);
-                showThanksModal(message.success);
-                messageStatus.remove();
-            })
-            .catch(()=> {
-                showThanksModal(message.failure);
-            })
-            .finally(() => {
-                form.reset();
-            });
+            postData('http://localhost:3000/requests', json)
+                .then(data => {
+                    console.log(data);
+                    showThanksModal(message.success);
+                    messageStatus.remove();
+                })
+                .catch(() => {
+                    showThanksModal(message.failure);
+                })
+                .finally(() => {
+                    form.reset();
+                });
         });
     }
 
@@ -291,57 +297,120 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
-   
     //Slider
 
     const slides = document.querySelectorAll('.offer__slide'),
-          prev = document.querySelector('.offer__slider-prev'),
-          next = document.querySelector('.offer__slider-next'),
-          total = document.querySelector('#total'),
-          current = document.querySelector('#current');
+        slider = document.querySelector('.offer__slider'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = getComputedStyle(slidesWrapper).width;
 
     let slideIndex = 1;
+    let offset = 0;
 
-    showSlides(slideIndex);
-
-    if(slides.length < 10){
+    if (slides.length < 10) {
         total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
     } else {
         total.textContent = slides.length;
+        current.textContent = slideIndex;
     }
 
-    function showSlides(n){
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
 
-        if(n > slides.length) {
-            slideIndex = 1;
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach((slide) => {
+        slide.style.width = width;
+    });
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+          dots = [];
+    indicators.classList.add('carousel-indicators');
+
+
+    slider.append(indicators);
+
+    for(let i = 0; i < slides.length; i++ ){
+        const dot = document.createElement('li');
+        dot.classList.add('dot');
+        dot.setAttribute('data-slide-to', i + 1);
+        indicators.append(dot);
+
+        if(i == 0) {
+            dot.style.opacity = 1;
         }
+        indicators.append(dot);
+        dots.push(dot);
+    }
 
-        if(n < 1){
-            slideIndex = slides.length;
-        }
-
-        slides.forEach((item) => {
-            item.style.display = 'none';
-        });
-
-        slides[slideIndex - 1].style.display = 'block'; 
-        if(slides.length < 10){
+    function setDotsIndex() {
+        if (slides.length < 10) {
             current.textContent = `0${slideIndex}`;
         } else {
             current.textContent = slideIndex;
         }
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
     }
 
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
     }
-
-    prev.addEventListener('click', () => {
-        plusSlides(-1);
-    });
 
     next.addEventListener('click', () => {
-        plusSlides(1);
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += deleteNotDigits(width);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        setDotsIndex();
     });
-    
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = deleteNotDigits(width) * (slides.length - 1);
+        } else {
+            offset -= deleteNotDigits(width);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        setDotsIndex();
+    });
+
+    dots.forEach((dot) => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+
+            offset = deleteNotDigits(width) * (slideTo - 1);
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            setDotsIndex();
+        });
+    });
+
 });
